a37573d171f00b6348dbb1f236a0ec157e91209d
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-03-28 16:59:56 -0400

    Actual last CSS fixes

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -53,11 +53,11 @@ class SessionForm extends React.Component {
                     <div className="inner-form-container">
                         <p className="session-form-desc">Enter your <b>username</b>, <b>password</b>, and <b>email</b>.</p>
                         <form className="session-form">
-                            <input type="text" onChange={this.handleInput("username")} className="session-form-input" placeholder="  username"/>
+                            <input type="text" onChange={this.handleInput("username")} className="session-form-input" placeholder="username"/>
                             <br></br>
-                            <input type="password" onChange={this.handleInput("password")} className="session-form-input" placeholder="  password"/>
+                            <input type="password" onChange={this.handleInput("password")} className="session-form-input" placeholder="password"/>
                             <br></br>
-                            <input type="text" onChange={this.handleInput("email")} value={this.state.email} className="session-form-input" placeholder="  you@example.com"/>
+                            <input type="text" onChange={this.handleInput("email")} value={this.state.email} className="session-form-input" placeholder="you@example.com"/>
                             <br></br>
                             <input type="submit" className="session-submit-button" value={this.props.formType} onClick={this.handleSubmit}/>
                             <input type="submit" className="session-submit-button" value="Demo User" onClick={this.demoUser} />
