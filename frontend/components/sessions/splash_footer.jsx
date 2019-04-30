02c9e02ed96f8a3d3cdd910533ae94a576af65f8
George Liu <georgeliu96@gmail.com>
2019-04-30 16:54:25 -0400

    Fix all splash links

--- frontend/components/sessions/splash_footer.jsx
+++ frontend/components/sessions/splash_footer.jsx
@@ -5,16 +5,28 @@ const SplashFooter = () => {
     <div className="splash-footer">
         <div></div>
         <div></div>
-        {/* <div class="splash-container"> */}
-            <a href="https://github.com/saphknight" className="github-link footer-link splash-container">
-                <img className="splash-icon" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
-                <div class="splash-overlay">
-                    <b className="github-label footer-label">Github</b>
-                </div>
-            </a>        
-        {/* </div> */}
-        <a href="https://www.linkedin.com/in/george-liu-740340183/" className="linkedin-link footer-link"><img className="linkedin-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Linkedin.svg/1024px-Linkedin.svg.png"/><b className="linkedin-label footer-label">LinkedIn</b></a>
-        <a href="https://saphknight.github.io/" className="site-link footer-link"><img src={window.personal} className="personal-icon"/><b className="site-label footer-label">Personal Site</b></a>
+        <a href="https://github.com/saphknight" className="github-link footer-link splash-container">
+            <img className="splash-icon" src="https://image.flaticon.com/icons/svg/25/25231.svg"/>
+            <div class="splash-overlay">
+                <b className="github-label footer-label">Github</b>
+            </div>
+        </a>        
+
+        <a href="https://www.linkedin.com/in/george-liu-740340183/" className="linkedin-link footer-link splash-container">
+            <img className="splash-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Linkedin.svg/1024px-Linkedin.svg.png"/>
+            <div class="splash-overlay">
+                <b className="linkedin-label footer-label">LinkedIn</b>
+            </div>
+        </a>
+
+        <a href="https://saphknight.github.io/" className="site-link footer-link splash-container">
+            <img src={window.personal} className="splash-icon"/>
+            <div class="splash-overlay">
+                <b className="footer-label">Portfolio</b>
+            </div>
+        </a>        
+
+
         <div></div>        
         <div></div>        
     </div>
