fdbe0e5c593e5417fa938dbe4b2c1c9d091ad1be
George Liu <georgeliu96@gmail.com>
2019-05-09 19:45:21 -0400

    Add protectedRoute to messages

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <div>
         <header>
             <Switch>
-                <Route path="/messages" component={ChatRoom} />
+                <ProtectedRoute path="/messages" component={ChatRoom} />
                 <Route path="/messages/:channelId" component={ChatRoom} />
                 <Route path="/" component={NavBar} />
             </Switch>
